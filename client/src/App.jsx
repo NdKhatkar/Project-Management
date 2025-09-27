d38fbcc99b050ad607c110941c782656fa49e9b2
NdKhatkar <149598939+NdKhatkar@users.noreply.github.com>
2025-09-27 21:11:20 +0530

    Add files via upload
    src folder files uploaded

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import Nevbar from './components/nevbar'
+export default function App(){
+    return (
+        <>
+            <Nevbar />
+            <div className="h-[100vh] w-full flex justify-center items-center"><h1>Welcome Back</h1></div>
+
+        </>
+    )
+}
